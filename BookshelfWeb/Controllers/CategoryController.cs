f8b7d9cb4f45b21c999731459372684de57d9646
Ali Pourpanah <alipoury035@gmail.com>
2024-07-09 00:37:07 +0330

    add category model/view/controller and initial database with EF migration.

---
+++ BookshelfWeb/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using BookshelfWeb.Controllers.Data;
+using BookshelfWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookshelfWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext dbContext) => _dbContext = dbContext;
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+    }
+}
---
+++ BookshelfWeb/Controllers/Data/dbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookshelfWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookshelfWeb.Controllers.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "history", DisplayOrder = 3 });
+        }
+    }
+}
