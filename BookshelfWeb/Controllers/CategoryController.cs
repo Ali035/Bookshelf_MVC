52087501fe8396e9844b0ba248cb7ed0ab34a312
Ali Pourpanah <alipoury035@gmail.com>
2024-07-10 18:38:27 +0330

    add category with its validation

--- BookshelfWeb/Controllers/CategoryController.cs
+++ BookshelfWeb/Controllers/CategoryController.cs
@@ -1,17 +1,35 @@
 ﻿using BookshelfWeb.Controllers.Data;
 using BookshelfWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookshelfWeb.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController(ApplicationDBContext dbContext) : Controller
     {
-        private readonly ApplicationDBContext _dbContext;
-        public CategoryController(ApplicationDBContext dbContext) => _dbContext = dbContext;
-        public IActionResult Index()
+        private readonly ApplicationDBContext _dbContext = dbContext;
+
+        public async Task<IActionResult> Index()
         {
-            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            List<Category> objCategoryList = await _dbContext.Categories.ToListAsync();
             return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                await _dbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return View();
         }
 
     }
--- BookshelfWeb/Models/Category.cs
+++ BookshelfWeb/Models/Category.cs
@@ -7,7 +7,9 @@ namespace BookshelfWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         public required string Name { get; set; }
+        [Range(0, 100)]
         public int? DisplayOrder { get; set; }
     }
 }
