59f1797fbc6d917eece7f1f38efff2e8de5b670f
Ali Pourpanah <alipoury035@gmail.com>
2024-07-11 02:16:18 +0330

    Complete CRUD opertaions of category.

--- BookshelfWeb/Controllers/CategoryController.cs
+++ BookshelfWeb/Controllers/CategoryController.cs
@@ -9,6 +9,11 @@ namespace BookshelfWeb.Controllers
     {
         private readonly ApplicationDBContext _dbContext = dbContext;
 
+        // Try not to use nested types unless there's a clear benefit.
+        public enum ActionModeEnum { Create, Update, Delete }
+        [ViewData]
+        public ActionModeEnum ActionMode { get; set; } = ActionModeEnum.Create;
+
         public async Task<IActionResult> Index()
         {
             List<Category> objCategoryList = await _dbContext.Categories.ToListAsync();
@@ -17,20 +22,81 @@ namespace BookshelfWeb.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            ActionMode = ActionModeEnum.Create;
+            return View("Create");
         }
         [HttpPost]
         public async Task<IActionResult> Create(Category category)
         {
+            ActionMode = ActionModeEnum.Create;
             if (ModelState.IsValid)
             {
                 _dbContext.Categories.Add(category);
                 await _dbContext.SaveChangesAsync();
 
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+
+            return View("Create");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category? category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            ActionMode = ActionModeEnum.Update;
+            return View("Create", category);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            ActionMode = ActionModeEnum.Update;
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                await _dbContext.SaveChangesAsync();
+
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View("Create");
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category? category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            ActionMode = ActionModeEnum.Delete;
+            return View("Create", category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeletePost(int? id)
+        {
+            if (id == null) return NotFound();
+            Category? category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null) return NotFound();
+
+            _dbContext.Categories.Remove(category);
+            await _dbContext.SaveChangesAsync();
+
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
     }
 }
